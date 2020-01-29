13b32a2f5d41eab925acac05ef6ced10ffa18c17
Konrad Piech <k9602@o2.pl>
2020-01-29 21:14:06 +0100

    sitecontroller and logout handler with flash messages

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,11 +3,13 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Http\Logout\LogoutSuccessHandlerInterface;
 
-class SecurityController extends AbstractController
+class SecurityController extends AbstractController implements LogoutSuccessHandlerInterface
 {
     /**
      * @Route("/login", name="app_login")
@@ -33,4 +35,11 @@ class SecurityController extends AbstractController
     {
         throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
+
+    public function onLogoutSuccess(Request $request)
+    {
+        $this->addFlash("success_logout" , "Wylogowano poprawnie");
+        return $this->render('site/index.html.twig');
+    }
+
 }
